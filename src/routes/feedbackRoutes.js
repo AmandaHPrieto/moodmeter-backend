953a8d3c2c1b45ce0f39b3ee9d8294ddcce5a4c1
EstelleDab <152979607+EstelleDab@users.noreply.github.com>
2025-03-18 19:50:55 +0100

    lien entre ue cliquée et url du feedback de l ue établi + feedback lié à l'utilisateur

--- src/routes/feedbackRoutes.js
+++ src/routes/feedbackRoutes.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const feedbackController = require('../controllers/feedbackController');
+const verifyToken = require( '../middleware/authMiddleware');
+
+
+
 
 // Route pour récupérer tous les feedbacks
 router.get('/feedback', feedbackController.getAllFeedback);
 
-// Route pour ajouter un feedback
-router.post('/feedback', feedbackController.addFeedback);
+// Route pour ajouter un feedback avec ajout de la verification du token utilisateur
+router.post('/feedback', verifyToken, feedbackController.addFeedback);
 
 // Route pour la moyenne des feedbacks d'une UE
 router.get('/feedback/moyenne', feedbackController.getAverageFeedback);
 
+//Route pour récupérer les données de l'UE
+router.get('/ue/:ueId',  feedbackController.getUeDetails);
 
 module.exports = router;
